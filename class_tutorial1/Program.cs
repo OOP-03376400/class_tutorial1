7fbcb597246fc83d6b638c80bf056c5face5c2d3
Koson Trachu <koson.trachu@gmail.com>
2016-08-12 17:03:07 +0700

    class
    Class 1

---
+++ class_tutorial1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ตัวอย่างที่ 1 การเขียนคลาสอย่างง่าย
+// คลาสอย่างง่าย ไม่มีอะไรเลย มีเพียงคำว่า class, ชื่อคลาส และวงเล็บปีกกา
+namespace class_tutorial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    // ระวัง อย่าไปเขียนไว้ในคลาส Program
+
+    class MyClass
+    {
+         // empty class.   
+    }
+ 
+}
